251ac0e69c29e22efe6d6618534bca1bebd5aa90
DaguDD <dagimdereje123@gmail.com>
2023-07-20 00:15:04 +0300

    Print comb3

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,5 @@
+output = ""
 for i in range(10):
     for j in range(i+1, 10):
-        print("{:02d}, {:02d}".format(i, j))
-print()
+        output += "{:02d}, {:02d}, ".format(i, j)
+print(output[:-2])
