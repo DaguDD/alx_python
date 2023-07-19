e2b59e2130461915d4fa2dfde2d689d77fd7e20f
DaguDD <dagimdereje123@gmail.com>
2023-07-20 00:57:34 +0300

    Print comb3

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,7 @@
-for i in range(10):
+for i in range(0, 9):
     for j in range(i+1, 10):
-        print("{:02d}, {:02d}".format(i, j))
-print()
+        if i == 0:
+            print("{:d}{:d}".format(i, j), end=", ")
+        else:
+            print("{:02d}".format(int(str(i)+str(j))), end=", ")
+print("89")
