01da3c3f3f4c8585dd112a8988865dc0f22aa36b
DaguDD <dagimdereje123@gmail.com>
2023-08-03 22:44:12 +0300

    4. Printing a square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,8 +1,15 @@
+""" 
+    Square class with private instance attribute size and public instance method
+"""
+
 class Square:
+    """
+        A class that defines a square by size
+    """
 
     def __init__(self, size=0):
-        self.__size = 0  
-        self.size = size  
+        self.__size = 0  # Default size is 0
+        self.size = size  # Use the property setter to set the size
 
     @property
     def size(self):
@@ -10,10 +17,10 @@ class Square:
 
     @size.setter
     def size(self, value):
-        
+        # Check if size is an integer
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        
+        # Check if size is non-negative
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
