72b41edd999142d27eaaad727426ff97c363e3ca
DaguDD <dagimdereje123@gmail.com>
2023-08-03 22:03:40 +0300

    2. Area of a square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,19 +1,10 @@
-""" 
-Class Square that defines a square by:
-(based on 1-square.py)
-"""
-
 class Square:
-    """
-    A class that defines a square by size.
-    """
-
+   
     def __init__(self, size=0):
-        self.__size = 0  # Default size is 0
+        self.__size = 0  
 
-        # Check if size is provided and is an integer
         if isinstance(size, int):
-            # Check if size is non-negative
+            
             if size >= 0:
                 self.__size = size
             else:
---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+class Square:
+    
+    def __init__(self, size=0):
+        self.__size = 0  # Default size is 0
+        self.size = size  # Use the property setter to set the size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        # Check if size is an integer
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        # Check if size is non-negative
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+        
