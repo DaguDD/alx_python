3cc25f04548e41832f09fc58977e77d8a6b8c2f7
DaguDD <dagimdereje123@gmail.com>
2023-08-11 18:15:02 +0300

    task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+"""
+Fetches https://alu-intranet.hbtn.io/status
+"""
+
 import requests
 
-url = 'https://alu-intranet.hbtn.io/status'
-response = requests.get(url)
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
+    response = requests.get(url)
 
-if response.status_code == 200:
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
-else:
-    print("Error: Failed to fetch the URL. Status code:", response.status_code)
-    
     
