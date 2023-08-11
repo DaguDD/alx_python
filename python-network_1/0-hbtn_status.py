2910ace59b5ab5d2c1b4e672dc55841e3a1e4a4b
DaguDD <dagimdereje123@gmail.com>
2023-08-11 18:06:24 +0300

    Response header value #1

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import requests
+
+url = 'https://alu-intranet.hbtn.io/status'
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
+else:
+    print("Error: Failed to fetch the URL. Status code:", response.status_code)
+    
